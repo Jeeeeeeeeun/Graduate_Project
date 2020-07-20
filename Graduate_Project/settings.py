39815a96a5c0ad587bc999ea281b5117dda8ea92
3jine33 <3jine33@likelion.org>
2020-07-20 16:29:14 +0900

    database changed to sqlite3

--- Graduate_Project/settings.py
+++ Graduate_Project/settings.py
@@ -77,13 +77,8 @@ WSGI_APPLICATION = 'Graduate_Project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django_db',
-        'USER': 'root',
-        'PASSWORD': 'sm1906',
-        'HOST': '127.0.0.1',
-        'PORT': '5433'
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 

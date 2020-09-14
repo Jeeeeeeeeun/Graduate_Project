4597c0d434bfbd679cb719879680fdb374b2af45
3jine33 <3jine33@likelion.org>
2020-09-14 22:14:28 +0900

    template css done

--- fontsapp/forms.py
+++ fontsapp/forms.py
@@ -5,3 +5,11 @@ class FontForm(forms.ModelForm):
     class Meta:
         model = Font
         fields=['phrase1', 'template_img', 'input_photo1', 'output_photo1']
+        widgets = {
+            'phrase1' : forms.TextInput(
+                attrs={
+                    'style' : 'width:800px; border:solid gray; border-radius:5px; padding:20px; text-align:center;font-family:"나눔고딕";font-size:19px;',
+                    'value' : '눈송이는 졸업을 꿈꾸지',
+                }
+            )
+        }
